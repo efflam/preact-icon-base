21f8cc18a4f9f9f7ad23cf97b7f244c75901d8b9
Efflam <Efflam.DANIEL@publicis-modem.fr>
2017-03-07 16:15:29 +0100

    use preact

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
-import { default as React, PropTypes } from 'react'
+import { h } from "preact";
 
-const IconBase = ({ children, color, size, style, ...props }, { reactIconBase = {} }) => {
-  const computedSize = size || reactIconBase.size || '1em'
+const IconBase = (
+  { children, color, size, style, ...props },
+  { preactIconBase = {} }
+) => {
+  const computedSize = size || preactIconBase.size || "1em";
   return (
     <svg
       children={children}
-      fill='currentColor'
-      preserveAspectRatio='xMidYMid meet'
+      fill="currentColor"
+      preserveAspectRatio="xMidYMid meet"
       height={computedSize}
       width={computedSize}
-      {...reactIconBase}
+      {...preactIconBase}
       {...props}
       style={{
-        verticalAlign: 'middle',
-        color: color || reactIconBase.color,
-        ...(reactIconBase.style || {}),
+        verticalAlign: "middle",
+        color: color || preactIconBase.color,
+        ...(preactIconBase.style || {}),
         ...style
       }}
     />
-  )
-}
+  );
+};
 
-IconBase.propTypes = {
-  color: PropTypes.string,
-  size: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number
-  ]),
-  style: PropTypes.object
-}
-
-IconBase.contextTypes = {
-  reactIconBase: PropTypes.shape(IconBase.propTypes)
-}
-
-export default IconBase
+export default IconBase;
